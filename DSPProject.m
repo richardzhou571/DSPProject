fd004d6aae1377a74a86dedb8938dea104847e87
Ahmed Hussain <ahh6qxu@virginia.edu>
2021-10-13 00:37:39 -0400

    read rgb image, convert to grayscale, create image vector

--- DSPProject.m
+++ DSPProject.m
@@ -1,35 +1,15 @@
-% Testing Sound library in MATLAB
-load gong.mat;
-%sound([1, 2, 3, -4, -3, -2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]/5);
+% read in RGB image
+color_image = imread('bladerunner.jpeg');
+imshow(color_image)
 
-% Testing image library in MATLAB
-corn_gray = imread('corn.tif', 3);
-meanIntensity = mean(corn_gray(:));
-corn_binary = corn_gray > meanIntensity;
+% convert RGB image to grayscale using rgb2gray()
+grayscale_image = rgb2gray(color_image);
+figure
+imshow(grayscale_image)
 
-banana = imread('banana.jpg');
-banana_grayscale = banana(:, :, 1)/3 + banana(:, :, 2)/3 + banana(:, :, 3)/3;
-banana_binary = banana_grayscale > mean(banana_grayscale(:));
+% push all image values to a row vector (MxN image to 1xM*N vector)
+image_vec = reshape(grayscale_image, 1, []);
+expected_dims = [1 size(grayscale_image,1)*size(grayscale_image,2)];
 
-strawberry = imread('strawberry.jpg');
-strawberry_grayscale = strawberry(:, :, 1)/3 + strawberry(:, :, 2)/3 + strawberry(:, :, 3)/3;
-strawberry_binary = strawberry_grayscale > mean(strawberry_grayscale(:));
-
-% Image a
-a = zeros(20);
-a(5:15, 5:15) = 1;
-
-% Image b
-b = ones(20);
-
-dtft_a = abs(dtft(a));
-for i=1:length(dtft_a(:,1))
-    sound(dtft_a(i, :));
-end
-
-dtft_b = abs(dtft(b));
-for i=1:length(dtft_b(:,1))
-    sound(dtft_b(i, :));
-end
-imshow(a)
-imshow(b)
+% sanity check for image vector
+assert(expected_dims == size(image_vec), "Image vector conversion failed!");
